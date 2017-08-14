db183c0dd541e5ddf4d728b5806311a5383f2477
Johnny <31002996+jmieses@users.noreply.github.com>
2017-08-14 14:27:53 +0200

    Update cw.m

--- cw.m
+++ cw.m
@@ -1,6 +1,6 @@
 function []=cw(bbb,P1,P2)
 writeDigitalPin(bbb,P2,0);
 writeDigitalPin(bbb,P1,1);
-pause(0.15);
+pause(0.15); % change time depending on motor setup. 
 writeDigitalPin(bbb,P1,0);
 end
