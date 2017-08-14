26c7b549d30d304a3f1f383c209d882d5b955f4f
Johnny <31002996+jmieses@users.noreply.github.com>
2017-08-14 14:27:06 +0200

    Update ccw.m

--- ccw.m
+++ ccw.m
@@ -1,6 +1,6 @@
 function []=ccw(bbb,P1,P2)
 writeDigitalPin(bbb,P1,0);
 writeDigitalPin(bbb,P2,1);
-pause(0.15);
+pause(0.15); % change time depending on motor setup.
 writeDigitalPin(bbb,P2,0);
 end
