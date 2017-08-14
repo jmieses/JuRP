3dce69663768ed90de744aa7843de94bf3a53f55
Johnny <31002996+jmieses@users.noreply.github.com>
2017-08-14 14:25:18 +0200

    Update Encoder_reading.m

--- Encoder_reading.m
+++ Encoder_reading.m
@@ -1,21 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This program run the motor upwards and downwards with the help of two functions: cw, and ccw. 
+% These two functions change the direction of the motor clockwise and counter-clockwise.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all
 close all
 clc
 
-bbb = beaglebone;
+bbb = beaglebone; % This line creates the connection between the BBB and MATLAB.
 
 P14 = 'P9_14';
 P21 = 'P9_21';
 P12 = 'P9_12';
 
-enablePWM(bbb, P14);
-writePWMDutyCycle(bbb, P14, 1);
-writePWMFrequency(bbb, P14, 4500);
-writePWMVoltage(bbb, P14, 3);
+enablePWM(bbb, P14); % enabling pin 14 to produce a pwm signal
+writePWMDutyCycle(bbb, P14, 1); % set the duty cycle of the signal range 0 (0%) to 1 (100%)
+writePWMFrequency(bbb, P14, 4500); % sets the frequency of the signal.
+writePWMVoltage(bbb, P14, 3); % voltage that produce the pwm signal (implicit duty cycle)
 
-configureDigitalPin(bbb,P21,'output');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% setting pin 21 and 12 to change direction of the motor. 
+configureDigitalPin(bbb,P21,'output'); 
 configureDigitalPin(bbb,P12,'output');
-% rotaryEncoder(bbb,PA,PB)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 counter = 0;
 
@@ -25,6 +32,7 @@ while(1)
     ccw(bbb,P21,P12);
    counter = counter + 1;
    if (counter == 10)
+   % shutting down the motor after the counter reach its limit. 
        writePWMVoltage(bbb, P14, 0);
        writeDigitalPin(bbb,'P9_21',0);
        writeDigitalPin(bbb,'P9_12',0);
